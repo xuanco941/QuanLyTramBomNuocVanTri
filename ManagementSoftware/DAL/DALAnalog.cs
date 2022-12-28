256bf343552f69a7ae0dc505850b6c6a48c7bb1b
xuanco941 <xuanco941@gmail.com>
2022-12-28 08:32:20 +0700

    save

--- ManagementSoftware/DAL/DALAnalog.cs
+++ ManagementSoftware/DAL/DALAnalog.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ManagementSoftware.Models.DuLieuMayPLC;
+using Microsoft.EntityFrameworkCore;
 
 namespace ManagementSoftware.DAL
 {
@@ -24,20 +26,19 @@ namespace ManagementSoftware.DAL
             await dbContext.SaveChangesAsync();
         }
 
-        //public static List<DataDoThi> GetListDataOn1Hour(string nameTinHieu)
-        //{
-        //    System.DateTime timeNow = DateTime.Now;
-        //    System.DateTime timeStart = timeNow.AddHours(-1);
-        //    while (timeStart.Date != timeNow.Date)
-        //    {
+        public static List<DataDoThi>? GetListDataOn1Hour(string nameTinHieu)
+        {
+            DataBaseContext dbContext = new DataBaseContext();
 
-        //    }    
+            List<DataDoThi> dataDoThi = new List<DataDoThi>();
+            System.DateTime timeNow = System.DateTime.Now;
+            System.DateTime timeStart = timeNow.AddHours(-1);
+            while (timeStart.Date != timeNow.Date)
+            {
+                DateTime count = timeStart.AddMinutes(2);
 
-        //}
+            }
+        }
     }
-    class DataDoThi
-    {
-        public DateTime dateTime { set; get; }
-        public double Value { get; set; }
-    }
+
 }
